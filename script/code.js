ac62962523c403293674b0993f8478a2dc8d12f7
Siya021 <siyamthandamazokwana@gmail.com>
2023-05-24 15:35:23 +0200

    functions

---
+++ script/code.js
@@ -0,0 +1,15 @@
+function greet() {
+    console.log('Hello my name is Siyamthanda')
+}
+greet();
+
+function multiply (numb1, numb2){
+    return numb1 * numb2
+
+}
+console.log(multiply(6,9));
+
+function tableofFive (number){
+    return number * 5
+}
+console.log(tableofFive(4))
